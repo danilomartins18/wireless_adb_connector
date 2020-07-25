adc6c1586611ab19408b758908b23584580babc9
danilomartins18 <danilomartins18@gmail.com>
2020-07-25 13:14:59 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Wireless ADB Connector
+
+- Devices ADB Connector via Wi-Fi
+
+## Features
+- Connect your device via ADB to your computer via Wi-Fi for the purpose of debugging projects in Flutter and perhaps other cross-platform projects (not tested) for Android.
+
+## How to use
+- Install Android SDK (Only the first time)
+- Set the path of adb into System Variables (Only the first time)
+- On your device check which WiFi IP (Only the first time or when your device's IP changes)
+- Connect the cable to your phone
+- Run the project
+- Disconnect the USB cable
+
+# References
+
+- [Flutter — Debug por wifi](https://medium.com/@jmellodev/flutter-debug-por-wifi-435d508a09cf)
+- [Reference Project](https://github.com/mousedoc/nox-adb-connector)
---
+++ wireless_adb_connector/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace wireless_adb_connector
+{
+    class Program
+    {
+        const string IPDEVICE = "192.168.0.100";
+
+        private static Process process = null;
+
+        private static Process Process
+        {
+            get
+            {
+                if (process == null)
+                {
+                    process = new Process();
+                    process.StartInfo.FileName = "cmd.exe";
+                    process.StartInfo.RedirectStandardInput = true;
+                    process.StartInfo.RedirectStandardOutput = true;
+                    process.StartInfo.CreateNoWindow = false;
+                    process.StartInfo.UseShellExecute = false;
+                    process.Start();
+                }
+
+                return process;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // INFO : Nox app player installed in drive D
+            var lines = new List<string>()
+            {
+                @"adb tcpip 5555",
+                @$"adb connect {IPDEVICE}:5555",
+            };
+
+            // Run and wait for exit
+            ProcessLines(lines);
+            Console.WriteLine("\r\nPress a key");
+            Console.ReadKey();
+        }
+
+        private static void ProcessLines(List<string> lines)
+        {
+            foreach (var line in lines)
+            {
+                Process.StandardInput.WriteLine(line);
+                Process.StandardInput.Flush();
+            }
+
+            Process.StandardInput.Close();
+            Process.WaitForExit();
+
+            var outputs = Process.StandardOutput.ReadToEnd().Split('\n');
+            Console.WriteLine(outputs[outputs.Length - 3]);
+        }
+    }
+}
